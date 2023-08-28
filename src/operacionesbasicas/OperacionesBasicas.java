188338fc7de13377c8056da6218fce1bc2f27353
SantiagoVargas <santiago19267@gmail.com>
2023-08-27 21:50:33 -0500

    Ejercicio con Operaciones Basicas en JAVA

---
+++ src/operacionesbasicas/OperacionesBasicas.java
@@ -0,0 +1,17 @@
+package operacionesbasicas;
+
+/**
+ *
+ * @author SENA
+ */
+public class OperacionesBasicas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Operaciones calculadora = new Operaciones();
+        calculadora.setVisible(true);
+    }
+    
+}
